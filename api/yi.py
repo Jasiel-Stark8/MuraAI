c27a9ef8e4f9374cbd615ec5dd176cb2dd1abe72
== <==>
2023-11-29 23:12:34 +0000

    Yi model setup

--- api/yi.py
+++ api/yi.py
@@ -7,14 +7,12 @@ model_path = '01-ai/Yi-34b-Chat'
 
 tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=False)
 
-# Since transformers 4.35.0, the GPT-Q/AWQ model can be loaded using AutoModelForCausalLM.
 model = AutoModelForCausalLM.from_pretrained(
     model_path,
     device_map="auto",
     torch_dtype='auto'
 ).eval()
 
-# Prompt content: "hi"
 messages = [
     {"role": "user", "content": "hi"}
 ]
@@ -23,5 +21,4 @@ input_ids = tokenizer.apply_chat_template(conversation=messages, tokenize=True, 
 output_ids = model.generate(input_ids.to('cuda'))
 response = tokenizer.decode(output_ids[0][input_ids.shape[1]:], skip_special_tokens=True)
 
-# Model response: "Hello! How can I assist you today?"
 print(response)
