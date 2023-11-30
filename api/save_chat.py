4a9afb63762e5d3fdc77d3e595096575c298d7a1
== <==>
2023-11-30 15:20:53 +0000

    add save chat endpoint

---
+++ api/save_chat.py
@@ -0,0 +1,34 @@
+"""Save Chat Message Module"""
+from flask import Blueprint, request, jsonify
+from models.chat_thread import ChatMessage
+from database import db
+
+save_chat_blueprint = Blueprint('save_chat_message', __name__)
+
+@save_chat_blueprint.route('/save_chat_message', methods=['POST'], strict_slashes=False)
+def save_chat_message():
+    """Save chat message to database"""
+    try:
+        data = request.get_json()
+        user_id = data.get('user_id')
+        message_content = data.get('message_content')
+        message_type = data.get('message_type')
+
+        if not user_id or not message_content or not message_type:
+            return jsonify({"message": "User ID, content, and type are required"}), 400
+
+        new_chat_message = ChatMessage(
+            user_id=user_id,
+            message_content=message_content,
+            message_type=message_type
+        )
+
+        db.session.add(new_chat_message)
+        db.session.commit()
+
+        # Return the ID of the new chat message for frontend confirmation
+        return jsonify({"message": "Chat Message Saved", "chat_message_id": new_chat_message.id}), 201
+
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"message": "An error occurred while saving the chat message", "error": str(e)}), 500
--- models/chat_thread.py
+++ models/chat_thread.py
@@ -4,13 +4,13 @@ from database import db
 from models.users import User
 
 class ChatMessage(db.Model):
-    """User chat schema"""
+    """User Chat Schema"""
     __tablename__ = 'chat_messages'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    message_content = db.Column(db.String(1000), nullable=False)
-    message_type = db.Column(db.String(50), nullable=False)  # 'user' or 'assistant'
-    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
+    message_content = db.Column(db.Text, nullable=False)
+    message_type = db.Column(db.String(50), nullable=False)
+    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False)
 
     def __init__(self, user_id, message_content, message_type, timestamp):
         self.user_id = user_id
