dac88db39551bc30737794afa1a2dd36b95adab7
== <==>
2023-11-30 00:22:32 +0000

    database model setup

--- api/yi.py
+++ api/yi.py
@@ -1,5 +1,5 @@
 from flask import Flask, Blueprint
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, LlamaTokenizer
 
 app = Blueprint( 'app', __name__)
 
@@ -18,7 +18,7 @@ messages = [
 ]
 
 input_ids = tokenizer.apply_chat_template(conversation=messages, tokenize=True, add_generation_prompt=True, return_tensors='pt')
-output_ids = model.generate(input_ids.to('cuda'))
+output_ids = model.generate(input_ids.to('cpu'))
 response = tokenizer.decode(output_ids[0][input_ids.shape[1]:], skip_special_tokens=True)
 
 print(response)
---
+++ models/users.py
@@ -0,0 +1,14 @@
+"""User Model for PostgreSQL database"""
+from flask_sqlalchemy import SQLAlchemy
+from database import db
+
+class Users(db.Model):
+    """User Model Schema"""
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(40), nullable=False, unique=True)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    def __init__(self, email, password_hash):
+        self.email = email
+        self.password_hash = password_hash
