c77e8406724f5a90ba0780facc3e3e530dc6c76a
== <==>
2023-11-30 14:01:57 +0000

    Database complete

---
+++ models/chat_thread.py
@@ -0,0 +1,19 @@
+"""Store user chat"""
+from flask_sqlalchemy import SQLAlchemy
+from database import db
+from models.users import User
+
+class ChatMessage(db.Model):
+    """User chat schema"""
+    __tablename__ = 'chat_messages'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    message_content = db.Column(db.String(1000), nullable=False)
+    message_type = db.Column(db.String(50), nullable=False)  # 'user' or 'assistant'
+    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    def __init__(self, user_id, message_content, message_type, timestamp):
+        self.user_id = user_id
+        self.message_content = message_content
+        self.message_type = message_type
+        self.timestamp = timestamp
--- models/users.py
+++ models/users.py
@@ -3,9 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from database import db
 
 class User(db.Model):
-    """User Model Schema"""
+    """User Schema"""
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(40), nullable=False, unique=True)
     password_hash = db.Column(db.String(255), nullable=False)
 
