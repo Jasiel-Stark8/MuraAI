009b2b30ff972c004789d3be5f7892fec0f4bcba
Christopher Seven Phiri <chris7phiri@gmail.com>
2023-12-01 15:38:34 +0200

    Finish chat screen (#3)
    * image updates
    
    * update scripts
    
    * clean up home
    
    * active nav
    
    * Update generate.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,80 +1,84 @@
-const state = {
-  lastId: null,
-};
+function chatBot() {
+  return {
+    botTyping: false,
+    messages: [],
+    lastIdx: null,
+    output: function (input) {
+      let text = input.trim();
+      // Add user message
+      this.messages.push({
+        from: "user",
+        text: text,
+      });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const chatMessages = document.getElementById("chat-messages");
-  const messageInput = document.getElementById("message-input");
-  const sendBtn = document.getElementById("send-btn");
-  const introSection = document.getElementById("intro-section");
-
-  sendBtn.addEventListener("click", async () => {
-    questionInput = messageInput.value;
-    introSection.style.display = "none";
-    appendUserMessage();
-    const response = await fetch("/api/chat", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ prompt: questionInput }),
-    });
-    const reader = response.body.getReader();
-    reader.read().then(function pump({ done, value }) {
-      console.log({ done, value });
-      if (done) {
-        state.lastId = null;
-        return;
+      this.scrollChat();
+      this.botTyping = true;
+      this.sendPromptRequest(
+        input,
+        (response) => {
+          if (response != null) {
+            let lastId = this.lastIdx;
+            if (lastId === null) {
+              const newidx = this.messages.push({
+                from: "bot",
+                text: response,
+              });
+              this.lastIdx = newidx - 1;
+            } else {
+              this.messages[
+                lastId
+              ].text = `${this.messages[lastId].text} ${response}`;
+            }
+            this.botTyping = false;
+          } else {
+            this.lastIdx = null;
+          }
+          this.scrollChat();
+        },
+        (error) => {
+          this.lastIdx = null;
+        }
+      );
+    },
+    sendPromptRequest: async function (prompt, onType, onError) {
+      try {
+        const response = await fetch("/api/chat", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ prompt: prompt }),
+        });
+        const reader = response.body.getReader();
+        reader.read().then(function pump({ done, value }) {
+          if (done) {
+            onType(null);
+            return;
+          }
+          if (value != null) {
+            var string = new TextDecoder().decode(value);
+            onType(string);
+          }
+          return reader.read().then(pump);
+        });
+      } catch (error) {
+        onError(error);
       }
-      if (value != null) {
-        var string = new TextDecoder().decode(value);
-        appendBotMessage(string, done);
+    },
+    scrollChat: function () {
+      const messagesContainer = document.getElementById("messagesContainer");
+      messagesContainer.scrollTop =
+        messagesContainer.scrollHeight - messagesContainer.clientHeight;
+      setTimeout(() => {
+        messagesContainer.scrollTop =
+          messagesContainer.scrollHeight - messagesContainer.clientHeight;
+      }, 100);
+    },
+    updateChat: function (target) {
+      if (target.value.trim()) {
+        this.output(target.value.trim());
+        target.value = "";
       }
-      return reader.read().then(pump);
-    });
-  });
-
-  function appendBotMessage(text) {
-    let messageDiv;
-    let lastId;
-    let isNew = false;
-    if (state.lastId === null) {
-      state.lastId = Math.random().toString(36).substring(7);
-      messageDiv = document.createElement("div");
-      lastId = state.lastId;
-      isNew = true;
-    } else {
-      messageDiv = document.getElementById(state.lastId);
-    }
-
-    if (isNew) {
-    messageDiv.className = `mb-2 flex flex-col w-full gap-6`;
-      messageDiv.innerHTML = `
-            <div class="flex flex-row gap-4 items-center">
-                <img class="bg-primary rounded-full w-8 h-8 p-2" src="/static/image/logo_small.png" alt="">
-                <span>Mura AI</span>
-            </div>
-            <p id="${lastId}">${text}</p>
-      `;
-      chatMessages.appendChild(messageDiv);
-    } else {
-      messageDiv.textContent = `${messageDiv.textContent} ${text}`;
-    }
-    // Scroll to the bottom to always show the latest message
-    chatMessages.scrollTop = chatMessages.scrollHeight;
-  }
-
-  function appendUserMessage() {
-    const messageDiv = document.createElement("div");
-    messageDiv.className = "mb-2 text-right";
-    messageDiv.innerHTML = `
-        <div class="flex flex-row gap-4 items-center">
-                <img class="bg-neutral-content rounded-full w-8 h-8 p-2" src="/static/image/user.png" alt="">
-                <span>You</span>
-            </div>
-            <p>${messageInput.value}</p>
-    `;
-    messageInput.value = "";
-    chatMessages.appendChild(messageDiv);
-  }
-});
+    },
+  };
+}
