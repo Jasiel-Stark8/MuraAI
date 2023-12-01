12edd0aac472428cda5b85b752fdd6b799d6e7d9
Christopher Seven Phiri <chris7phiri@gmail.com>
2023-12-01 18:30:27 +0200

    add small temp history

--- api/yi.py
+++ api/yi.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify, Blueprint, request, Response
 from flask_socketio import SocketIO
 from dotenv import load_dotenv
 import replicate
+import uuid
 
 load_dotenv()
 REPLICATE_API_TOKEN = os.getenv('REPLICATE_API_TOKEN')
@@ -11,13 +12,19 @@ app = Flask(__name__)
 socketio = SocketIO(app)
 chat = Blueprint('chat', __name__)
 
-def generate_response_function(prompt):
+
+user_conversations = {}
+
+def generate_response_function(session_id,prompt):
+    conversation_history = user_conversations.get(session_id, [])
+    newPrompt = "\n".join(conversation_history + [f"user\n{prompt}\nassistant"])
+    print(conversation_history)
     output = replicate.run(
             "01-ai/yi-6b-chat:14efadfaf772f45ee74c14973007cbafab3ccac90169ec96a9fc7a804253535d",
             input={
                 "top_k": 50,
                 "top_p": 0.8,
-                "prompt": prompt,
+                "prompt": newPrompt,
                 "temperature": 0.3,
                 "max_new_tokens": 500,
                 "prompt_template": "system\nYou are Mura, an AI health assistant designed to provide support with general health queries, \
@@ -32,7 +39,16 @@ def generate_response_function(prompt):
             }
         )
 
+    isFirst = True
     for char in output:
+        conversation_history = user_conversations.get(session_id, [])
+        if isFirst:
+            isFirst = False
+            user_conversations[session_id] = conversation_history + [f"user\n{prompt}", f"assistant\n{char}"]
+        else:
+            oldArry = user_conversations[session_id]
+            oldArry[-1] = oldArry[-1] + char
+            user_conversations[session_id] = oldArry
         yield char
 
 
@@ -41,7 +57,8 @@ def generate_response_function(prompt):
 def mura_chat():
     data = request.get_json()
     prompt = data.get('prompt', '')
-    return generate_response_function(prompt),{ "Content-Type":'text/event-stream'}
+    session_id = request.headers.get('Session-Id', str(uuid.uuid4()))
+    return generate_response_function(session_id,prompt),{ "Content-Type":'text/event-stream', "Session-Id": session_id}
 
 @socketio.on('start_chat')
 def handle_start_chat(data):
